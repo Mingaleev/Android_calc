ad69091fd25ae8997fed552890a62819e740a405
Mingaleev <art.mingaleev@yandex.ru>
2021-02-14 21:47:02 +0300

    Добавил меню в Toolbar и реализовал переход

--- app/src/main/java/com/minga/android_calc/MainActivity.java
+++ app/src/main/java/com/minga/android_calc/MainActivity.java
@@ -1,10 +1,15 @@
 package com.minga.android_calc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -37,6 +42,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initButton();
         tv_calc = findViewById(R.id.tv_calc);
         calc = new Calc();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_settings){
+            Intent intent = new Intent(this, Settings_calc.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initButton() {
