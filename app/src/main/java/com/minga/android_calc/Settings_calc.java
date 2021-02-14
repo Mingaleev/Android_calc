2ea38789a2d9a5e5de567687f1c46ba44af0a015
Mingaleev <art.mingaleev@yandex.ru>
2021-02-14 15:14:12 +0300

    Добавил новое активити

---
+++ app/src/main/java/com/minga/android_calc/Settings_calc.java
@@ -0,0 +1,14 @@
+package com.minga.android_calc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Settings_calc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_calc);
+    }
+}
